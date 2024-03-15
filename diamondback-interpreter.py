f0f930988c3e2df24ba691c80a0031de1e466aad
Zander <158775116+WolfTheDeveloper@users.noreply.github.com>
2024-03-14 20:36:58 -0400

    Optimization and Fixes Part 2
    1. Library Changes
    
    Libraries are now located in the 'libs' folder and variables are saved between libraries if you use '[varcopy]' after the library name. For example: 'import library [varcopy]' will copy all of the variables from the imported library. Libraries within libraries also work.
    
    2. Function Changes
    
    Functions are now fixed and can be called properly. When defining a function, you now put '()' at the end of the function name to define a function. plang tags also work inside functions.

--- diamondback-interpreter.py
+++ diamondback-interpreter.py
@@ -1,6 +1,7 @@
 import operator
 import sys
 
+
 class Interpreter:
     def __init__(self):
         self.variables = {}
@@ -12,7 +13,7 @@ class Interpreter:
         self.allow_library_vars = False
 
     def run_from_file(self, filename):
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             code = file.readlines()
             self.run(code)
 
@@ -23,65 +24,70 @@ class Interpreter:
 
         for line in lines:
             stripped_line = line.strip()
-             
+
             if stripped_line == "[plang python]":
                 in_python_block = True
-                continue   
-             
+                continue
+
             if stripped_line == "[endplang]" and in_python_block:
                 in_python_block = False
-                code_to_execute.append(('python', python_code.strip()))   
-                python_code = ""   
+                code_to_execute.append(("python", python_code.strip()))
+                python_code = ""
                 continue
 
             if in_python_block:
-                python_code += line + '\n'   
+                python_code += line + "\n"
             else:
-                 
+
                 if self.in_function_definition:
-                    if stripped_line == 'endf':
-                        self.functions[self.current_function_name] = self.current_function_body
+                    if stripped_line == "endf":
+                        self.functions[self.current_function_name] = (
+                            self.current_function_body
+                        )
                         self.in_function_definition = False
                         self.current_function_name = ""
                         self.current_function_body = []
                     else:
                         self.current_function_body.append(stripped_line)
-                else:   
-                    code_to_execute.append(('diba', stripped_line))
-         
+                else:
+                    code_to_execute.append(("diba", stripped_line))
+
         for code_type, code in code_to_execute:
-            if code_type == 'python':
+            if code_type == "python":
                 self.execute_python_code(code)
-            elif code_type == 'diba':
+            elif code_type == "diba":
                 self.parse_line(code)
 
     def execute_python_code(self, python_code):
-        local_scope = {**self.variables}
-        global_scope = {"__builtins__": __builtins__, "print": self.custom_print}
+        if not self.in_function_definition:
+            local_scope = {**self.variables}
+            global_scope = {"__builtins__": __builtins__, "print": self.custom_print}
 
-        try:
-            exec(python_code, global_scope, local_scope)
-             
-            self.variables.update(local_scope)
-             
-            for var in local_scope:
-                if callable(local_scope[var]):
-                    self.functions[var] = local_scope[var]
+            try:
+                exec(python_code, global_scope, local_scope)
 
-        except Exception as e:
-            print(f"Error executing embedded Python code: {e}")
+                self.variables.update(local_scope)
+
+                for var in local_scope:
+                    if callable(local_scope[var]):
+                        self.functions[var] = local_scope[var]
+
+            except Exception as e:
+                print(f"Error executing embedded Python code: {e}")
+        else:
+            return
 
     def custom_print(self, *args, **kwargs):
         print(*args, **kwargs)
 
     def parse_line(self, line):
-        if not line or line.startswith('//'):
+        if not line or line.startswith("//"):
             return
-        if line.startswith('def ') and line.endswith('()'):
+        if line.startswith("def ") and line.endswith("()"):
             self.handle_function_definition(line[4:-2])
             return
         if self.in_function_definition:
-            if line == 'endf':
+            if line == "endf":
                 self.in_function_definition = False
                 self.functions[self.current_function_name] = self.current_function_body
                 self.current_function_name = ""
@@ -89,39 +95,40 @@ class Interpreter:
             else:
                 self.current_function_body.append(line)
             return
-        
-        if line.startswith('var '):
-            var_name, var_value = line[4:].split('=')
-            self.variables[var_name.strip()] = self.evaluate_expression(var_value.strip())
-        elif line.startswith('print(') and line.endswith(')'):
+
+        if line.startswith("var "):
+            var_name, var_value = line[4:].split("=")
+            self.variables[var_name.strip()] = self.evaluate_expression(
+                var_value.strip()
+            )
+        elif line.startswith("print(") and line.endswith(")"):
             self.handle_print(line[6:-1])
-        elif line.startswith('import ') and line.endswith(" [varcopy]"):
+        elif line.startswith("import ") and line.endswith(" [varcopy]"):
             library_name = line[7:-10].strip()
             self.handle_import(library_name, True)
-        elif line.startswith('import '):
-            library_name = line[7:].strip()   
+        elif line.startswith("import "):
+            library_name = line[7:].strip()
             self.handle_import(library_name, False)
-        elif '=' in line:
+        elif "=" in line:
             self.handle_assignment(line)
-        elif line.endswith('()') and not line.startswith('def '):
-            
+        elif line.endswith("()") and not line.startswith("def "):
             func_name = line[:-2].strip()
             if func_name in self.functions:
                 self.handle_function_call(func_name)
             else:
                 print(f"Undefined function: {func_name}")
-        elif line.startswith('[plang') and line.endswith("]"):
+        elif line.startswith("[plang") and line.endswith("]"):
+            # self.execute_python_code(line)
             return
-        elif line.startswith('[endplang]'):
+        elif line.startswith("[endplang]"):
             return
         else:
             print("Invaild Syntax.")
             sys.exit(1)
-         
 
     def evaluate_expression(self, expression):
         tokens = expression.split()
-         
+
         if len(tokens) == 1:
             token = tokens[0]
             try:
@@ -131,49 +138,58 @@ class Interpreter:
                     return token[1:-1]
                 return self.variables.get(token, token)
 
-        while '+' in tokens or '-' in tokens or '*' in tokens or '/' in tokens:
+        while "+" in tokens or "-" in tokens or "*" in tokens or "/" in tokens:
             for i, token in enumerate(tokens):
-                if token in ['+', '-', '*', '/']:
-                    lhs = self.evaluate_expression(' '.join(tokens[:i]))
-                    rhs = self.evaluate_expression(' '.join(tokens[i+1:]))
-                    op = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}[token]
+                if token in ["+", "-", "*", "/"]:
+                    lhs = self.evaluate_expression(" ".join(tokens[:i]))
+                    rhs = self.evaluate_expression(" ".join(tokens[i + 1 :]))
+                    op = {
+                        "+": operator.add,
+                        "-": operator.sub,
+                        "*": operator.mul,
+                        "/": operator.truediv,
+                    }[token]
                     return op(lhs, rhs)
 
         raise ValueError(f"Invalid expression: {expression}")
 
-    def handle_print(self, line):  
+    def handle_print(self, line):
         value = self.evaluate_expression(line)
         print(value)
 
     def handle_assignment(self, line):
-        var_name, value = line.split('=')
+        var_name, value = line.split("=")
         var_name = var_name.strip()
         value = value.strip()
         self.variables[var_name] = value
 
     def handle_function_definition(self, line):
-        func_name, _, remainder = line.partition(' ')
+        func_name, _, remainder = line.partition(" ")
         self.current_function_name = func_name.strip()
         self.in_function_definition = True
-         
+
         if remainder:
-            print(f"Error: Unexpected code after function definition on the same line: {remainder.strip()}")
+            print(
+                f"Error: Unexpected code after function definition on the same line: {remainder.strip()}"
+            )
 
     def handle_function_call(self, function_name):
         if function_name in self.functions and not self.in_function_definition:
             function_body = self.functions[function_name]
-             
+
             for func_line in function_body:
                 self.parse_line(func_line)
         else:
-            print(f"Attempt to execute function '{function_name}' during its definition or function is undefined.")
+            print(
+                f"Attempt to execute function '{function_name}' during its definition or function is undefined."
+            )
 
     def handle_import(self, library_name, x):
         if library_name in self.imported_libraries:
             return
 
         try:
-            with open(f'libs/{library_name}.dbl', 'r') as file:
+            with open(f"libs/{library_name}.dbl", "r") as file:
                 library_code = file.readlines()
 
             current_vars = self.variables.copy()
@@ -181,14 +197,14 @@ class Interpreter:
             current_in_func_def = self.in_function_definition
             current_func_name = self.current_function_name
             current_func_body = self.current_function_body.copy()
-             
+
             self.run(library_code)
 
-            if x == True: 
+            if x == True:
                 self.variables.update(current_vars)
             else:
                 self.variables = current_vars
-            self.functions.update(current_funcs)   
+            self.functions.update(current_funcs)
             self.in_function_definition = current_in_func_def
             self.current_function_name = current_func_name
             self.current_function_body = current_func_body
@@ -199,14 +215,14 @@ class Interpreter:
             print(f"Library not found: {library_name}")
             sys.exit(1)
 
- 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python main.py <filename.diba> or diba <filename.diba>")
         sys.exit(1)
 
     filename = sys.argv[1]
-    if not filename.endswith('.diba'):
+    if not filename.endswith(".diba"):
         print("Error: File must have a .diba extension")
         sys.exit(1)
 
