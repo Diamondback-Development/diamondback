3874f8557691ee69ad3411e55e230eb9befd58e5
Zander <158775116+WolfTheDeveloper@users.noreply.github.com>
2024-03-18 18:29:37 -0400

    Update String Definition

--- diamondback-interpreter.py
+++ diamondback-interpreter.py
@@ -160,9 +160,7 @@ class Interpreter:
             try:
                 return int(token)
             except ValueError:
-                if (token.startswith('"') and token.endswith('"')) or (
-                    token.startswith("'") and token.endswith("'")
-                ):
+                if (token.startswith('"') and token.endswith('"')) or (token.startswith("'") and token.endswith("'")) or token.startswith('`') and token.endswith('`'):
                     return token[1:-1]
                 elif token in self.variables:
                     return self.variables[token]
